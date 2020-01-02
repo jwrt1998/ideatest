05295a7cf93610552a70be58f6fab26b3719008a
JWRT <JWRT@163.com>
2020-01-02 15:35:14 +0800

    测试IDEA提交工程

--- algorithm/blue_test/src/cn/jwrt/blue1123/GitTest.java
+++ algorithm/blue_test/src/cn/jwrt/blue1123/GitTest.java
@@ -3,5 +3,7 @@ package cn.jwrt.blue1123;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
     }
 }
